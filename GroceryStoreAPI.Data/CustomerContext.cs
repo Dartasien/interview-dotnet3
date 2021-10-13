e46f6a8012cc34dd28797f24013140a5f2178893
Feildon Hampton <hamptfe@hotmail.com>
2021-10-13 19:35:06 -0400

    add base ef core implementation with sqlite and migrations

---
+++ GroceryStoreAPI.Data/CustomerContext.cs
@@ -0,0 +1,21 @@
+﻿using GroceryStoreAPI.Data.Configurations;
+using GroceryStoreAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryStoreAPI.Data
+{
+    public class CustomerContext : DbContext
+    {
+        Customer Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Filename=GroceryStoreCustomers.sqlite3");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        }
+    }
+}
